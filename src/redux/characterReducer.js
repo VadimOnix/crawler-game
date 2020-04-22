f3d50d7371bc3eb95cb65fc79c64c4a457d466d7
Vadim Korolev <vkorolev1506@gmail.com>
2020-04-22 18:29:33 +0300

    dev: animate character movement

--- src/redux/characterReducer.js
+++ src/redux/characterReducer.js
@@ -1,29 +1,59 @@
-import playerSprite from '../assets/img/character/Female_Character.png'
+import playerSprite from '../assets/img/character/Female_Character.png';
 
 const MOVE_TO = 'MOVE-TO';
+const WALK_INDEX_UPDATE = 'WALK-INDEX-UPDATE';
+const PREV_DIRECTION_UPDATE = 'PREV-DIRECTION-UPDATE';
 
 let initialState = {
-    position : [0,0],
+    position: [0, 0],
     sprite: playerSprite,
+    spritePosition: [0, 0],
+    walkIndex: 0,
+    prevDirection: 'S'
 };
 
 const characterReducer = (state = initialState, action) => {
     switch (action.type) {
+        case PREV_DIRECTION_UPDATE:
+            return {
+                ...state,
+                prevDirection: action.prevDirection,
+            };
         case MOVE_TO:
             return {
                 ...state,
                 position: action.position,
+                spritePosition: action.spritePosition,
+            };
+        case WALK_INDEX_UPDATE:
+            return {
+                ...state,
+                walkIndex: action.index
             };
         default:
             return state;
     }
 };
 
-export const moveTo = (position) => {
+export const prevDirectionUpdate = (prevDirection) => {
+    return {
+        type: PREV_DIRECTION_UPDATE,
+        prevDirection
+    }
+};
+
+export const walkIndexUpdate = (index) => {
+    return {
+        type: WALK_INDEX_UPDATE,
+        index
+    }
+};
+
+export const moveTo = (position, spritePosition) => {
     return {
         type: MOVE_TO,
-        position
-    }
+        position, spritePosition
+    };
 };
 
 export default characterReducer;
